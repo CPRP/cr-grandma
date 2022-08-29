2962121c774923056675f83308df21d59d7fd6df
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2022-08-29 17:13:15 -0400

    updated location

--- config.lua
+++ config.lua
@@ -19,12 +19,12 @@ Config.Framework = {
 }
 
 Config.IllegalMedicalTarget = {
-    Coords = vector3(2441.89, 4985.82, 51.56), -- Coords for the Target
+    Coords = vector3(320.12, -193.99, 54.23), -- Coords for the Target
     Width = 0.5, -- Width for the Target
     Length = 0.5, -- Length for the Target
     Heading = 0, -- Heading for the Target
-    minZ = 50.56, -- Minimum Z for the Target
-    maxZ = 52.36, -- Maximum Z for the Target
+    minZ = 53.56, -- Minimum Z for the Target
+    maxZ = 55.36, -- Maximum Z for the Target
     DebugPoly = false, -- Whether you want the Target Poly's Enabled
     TargetDistance = 1.5, -- Distance in which you can Target the Medical Aid
     TargetLabel = "Speak with Grandma about your Health Issues", -- Target Label Description | Text you see for the Target Label
@@ -39,25 +39,25 @@ Config.IllegalMedicalTarget = {
 }
 
 Config.IllegalMedical = {
-    Coords = vector4(2441.8945, 4985.8208, 51.5649, 184.1566), -- Entire Vector4 Coordinate
+    Coords = vector4(320.12, -193.99, 54.23, 15.03), -- Entire Vector4 Coordinate
     PedModel = 'cs_mrs_thornhill', -- Ped Hash (https://docs.fivem.net/docs/game-references/ped-models/)
     PedName = "Grandma", -- Ped Name
-    PaymentType = 'cash', -- 'cash' = QBCore Player Cash Money | 'bank' = QBCore Player Bank Money | 'crypto' = QBCore Player Crypto Money | 'item' = QBCore Shared Item
-    PaymentCost = 0, -- Amount of Money | If you aren't using a Money Type(cash, bank, or crypto) than you can remove this Configuration
+    PaymentType = 'bank', -- 'cash' = QBCore Player Cash Money | 'bank' = QBCore Player Bank Money | 'crypto' = QBCore Player Crypto Money | 'item' = QBCore Shared Item
+    PaymentCost = 500, -- Amount of Money | If you aren't using a Money Type(cash, bank, or crypto) than you can remove this Configuration
     Item = nil, -- 'ItemName' | ItemName = QBCore Shared Item Name | If you aren't using an Item, you can remove this Configuration and the one below.
     ItemAmount = 0, -- Amount of Items
     OkOkNotificationTitle = "Grandma Aid",
     Minigame = {
-        Enabled = true,
-        PSUI = true,
+        Enabled = false,
+        PSUI = false,
         QBCoreSkillBar = false,
         QBCoreNPInspiredLock = false
     }
 }
 
 Config.MedicalAidInjuries = {
-    PaymentType = 'cash', -- 'cash' = QBCore Player Cash Money | 'bank' = QBCore Player Bank Money | 'crypto' = QBCore Player Crypto Money | 'item' = QBCore Shared Item
-    PaymentCost = 0, -- Amount of Money | If you aren't using a Money Type(cash, bank, or crypto) than you can remove this Configuration
+    PaymentType = 'bank', -- 'cash' = QBCore Player Cash Money | 'bank' = QBCore Player Bank Money | 'crypto' = QBCore Player Crypto Money | 'item' = QBCore Shared Item
+    PaymentCost = 500, -- Amount of Money | If you aren't using a Money Type(cash, bank, or crypto) than you can remove this Configuration
     Item = nil, -- 'ItemName' | ItemName = QBCore Shared Item Name | If you aren't using an Item, you can remove this Configuration and the one below.
     ItemAmount = 0, -- Amount of Items
 }
