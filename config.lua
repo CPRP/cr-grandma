a02b866e1b96e11e4cb33dc62da92fa935239548
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2023-01-21 21:02:39 -0500

    updated

--- config.lua
+++ config.lua
@@ -22,12 +22,12 @@ Config.Framework = {
 }
 
 Config.IllegalMedicalTarget = {
-    Coords = vector3(4963.16, -5108.32, 2.98), -- Coords for the Target
+    Coords = vector3(321.4, -193.46, 54.23), -- Coords for the Target
     Width = 0.5, -- Width for the Target
     Length = 0.5, -- Length for the Target
-    Heading = 112.51, -- Heading for the Target
-    minZ = 0.56, -- Minimum Z for the Target
-    maxZ = 4.36, -- Maximum Z for the Target
+    Heading = 22.63, -- Heading for the Target
+    minZ = 52.23, -- Minimum Z for the Target
+    maxZ = 56.23, -- Maximum Z for the Target
     DebugPoly = false, -- Whether you want the Target Poly's Enabled
     TargetDistance = 1.5, -- Distance in which you can Target the Medical Aid
     TargetLabel = "Speak with Grandma about your Health Issues", -- Target Label Description | Text you see for the Target Label
@@ -42,7 +42,7 @@ Config.IllegalMedicalTarget = {
 }
 
 Config.IllegalMedical = {
-    Coords = vector4(4963.16, -5108.32, 2.98, 112.51), -- Entire Vector4 Coordinate
+    Coords = vector4(321.4, -193.46, 54.23, 22.63), -- Entire Vector4 Coordinate
     PedModel = 'cs_mrs_thornhill', -- Ped Hash (https://docs.fivem.net/docs/game-references/ped-models/)
     PedName = "Grandma", -- Ped Name
     PaymentType = 'bank', -- 'cash' = QBCore Player Cash Money | 'bank' = QBCore Player Bank Money | 'crypto' = QBCore Player Crypto Money | 'item' = QBCore Shared Item
